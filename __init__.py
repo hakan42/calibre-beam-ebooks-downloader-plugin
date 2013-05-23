4c746c91bb21a668914ccd4277f3941690bbc1e2
Hakan Tandogan <hakan@gurkensalat.com>
2013-05-23 19:33:52 +0200

    More boilerplate to kick off development

---
+++ __init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = '2013, Hakan Tandogan <hakan at gurkensalat.com>'
+__docformat__ = 'restructuredtext en'
+
+# The class that all Interface Action plugin wrappers must inherit from
+from calibre.customize import InterfaceActionBase
+
+# Apparently the name for this class doesn't matter
+class Downloader(InterfaceActionBase):
+
+    name = 'Beam EBooks Downloader'
+    actual_plugin = 'calibre_plugins.beam_ebooks_downloader.action:BeamEbooksDownloaderAction'
+    description = _('UI plugin to download ebooks from Beam Ebooks.')
+    supported_platforms = ['windows', 'osx', 'linux']
+    author = 'Hakan Tandogan'
+
+    version = (1, 0, 0)
+
+    # Minium is 0.9.23 for the CLI support from this version on
+    minimum_calibre_version = (0, 9, 23)
---
+++ action.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = '2013, Hakan Tandogan <hakan at gurkensalat.com>'
+__docformat__ = 'restructuredtext en'
+
+# The class that all Interface Action plugin wrappers must inherit from
+from calibre.customize import InterfaceActionBase
+
+class BeamEbooksDownloaderAction(InterfaceActionBase):
+
+    # Stuff starts here....
+    pass
