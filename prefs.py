5bb64496ffcbdd060378f970ffef06db23ec1b8d
Hakan Tandogan <hakan@gurkensalat.com>
2013-05-24 20:30:04 +0200

    Provide some accessors to the prefs values

--- prefs.py
+++ prefs.py
@@ -9,7 +9,7 @@ __docformat__ = 'restructuredtext en'
 
 from calibre.utils.config import JSONConfig
 
-STORE_SCHEMA_VERSION = 'SchemaVersion'
+SCHEMA_VERSION = 'SchemaVersion'
 DEFAULT_SCHEMA_VERSION = 1.0
 
 # This is where all preferences for this plugin will be stored
@@ -22,9 +22,9 @@ plugin_prefs.defaults['hello_world_msg'] = 'Hello, World!'
 def migrate_config_if_required():
     # Contains code for migrating versions of json schema
     # Make sure we store our schema version in the file
-    schema_version = plugin_prefs.get(STORE_SCHEMA_VERSION, 0)
+    schema_version = plugin_prefs.get(SCHEMA_VERSION, 0)
     if schema_version != DEFAULT_SCHEMA_VERSION:
-        plugin_prefs[STORE_SCHEMA_VERSION] = DEFAULT_SCHEMA_VERSION
+        plugin_prefs[SCHEMA_VERSION] = DEFAULT_SCHEMA_VERSION
 
 
 class PrefsFacade():
@@ -34,6 +34,24 @@ class PrefsFacade():
         self.libraryid = None
         self.plugin_prefs = plugin_prefs
         self.passed_db = passed_db
+
+
+    def _get_prefs(self):
+        return self.plugin_prefs
+
+
+    def __getitem__(self, k):
+        prefs = self._get_prefs()
+        # if k not in prefs:
+        # pulls from default_prefs.defaults automatically if not set
+        # in default_prefs
+        # return self.default_prefs[k]
+        return prefs[k]
+
+
+    def __setitem__(self, k, v):
+        prefs = self._get_prefs()
+        prefs[k]=v
 
 
     def save(self):
