4a08cdf75c39960e7c0bc5c65af69ae0d06cb07f
Hakan Tandogan <hakan@gurkensalat.com>
2013-06-20 09:29:38 +0200

    #20: Shell script to be run from cron

---
+++ examples/run-beam-ebooks-downloader.sh
@@ -0,0 +1,16 @@
+#! /bin/sh -x
+
+export CALIBRE_DEVELOP_FROM=${HOME}/github/calibre/src
+export DISPLAY=:1
+
+LOGDIR=${HOME}/Dropbox/Calibre/_plugin_logs/beam-ebooks-downloader/$(date +%Y%m)/$(date +%Y%m%d)
+
+mkdir -p ${LOGDIR}
+
+calibre-debug \
+    --run-plugin "Beam EBooks Downloader" \
+    > ${LOGDIR}/stdout.txt \
+    2> ${LOGDIR}/stderr.txt
+
+mv /tmp/calibre-beam-ebooks-downloader-plugin ${LOGDIR}
+
