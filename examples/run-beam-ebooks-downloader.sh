c263ed1c80405fcc42cf0ae20458ec970cb8019b
Hakan Tandogan <hakan@gurkensalat.com>
2013-06-22 09:42:05 +0200

    #20: Allow for multiple daily runs

--- examples/run-beam-ebooks-downloader.sh
+++ examples/run-beam-ebooks-downloader.sh
@@ -3,7 +3,7 @@
 export CALIBRE_DEVELOP_FROM=${HOME}/github/calibre/src
 export DISPLAY=:1
 
-LOGDIR=${HOME}/Dropbox/Calibre/_plugin_logs/beam-ebooks-downloader/$(date +%Y%m)/$(date +%Y%m%d)
+LOGDIR=${HOME}/Dropbox/Calibre/_plugin_logs/beam-ebooks-downloader/$(date +%Y%m)/$(date +%Y%m%d-%H%M)
 
 mkdir -p ${LOGDIR}
 
