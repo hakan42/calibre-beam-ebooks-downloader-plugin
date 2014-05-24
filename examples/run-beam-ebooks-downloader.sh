a37b015e5a9c2b9be0770d5f6d6900f07eb2fbbc
Hakan Tandogan <hakan@gurkensalat.com>
2014-05-24 13:12:59 +0200

    Always use the freshest version of the plugin

--- examples/run-beam-ebooks-downloader.sh
+++ examples/run-beam-ebooks-downloader.sh
@@ -9,6 +9,10 @@ LOGDIR=${LOGDIR}/_plugin_logs/beam-ebooks-downloader/$(date +%Y%m)/$(date +%Y%m%
 
 mkdir -p ${LOGDIR}
 
+(cd ${HOME}/github/calibre-beam-ebooks-downloader-plugin && ant test) \
+    > ${LOGDIR}/ant-stdout.txt
+    2> ${LOGDIR}/ant-stderr.txt
+
 calibre-debug \
     --run-plugin "Beam EBooks Downloader" \
     > ${LOGDIR}/stdout.txt \
